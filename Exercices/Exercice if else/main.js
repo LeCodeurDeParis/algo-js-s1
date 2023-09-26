ee9e38e08a1aa62cea719fc5262b13cda7147ce8
LeCodeurDeParis <Pl.Baillion@gmail.com>
2023-09-26 17:47:00 +0200

    Adding Ex.1 Ex.2 Ex.Bonus

---
+++ Exercices/Exercice if else/main.js
@@ -0,0 +1,11 @@
+if (a == b) {
+    console.log("c'est égal")
+}
+
+if (c < b && a+c != 3) {
+    console.log("ok")
+}
+
+else{
+    console.log("raté")
+}
---
+++ learning/main.js
@@ -0,0 +1,5 @@
+//alert ("Ectoplasme tu veux quitter mon site ??")
+
+//console.log() //Affiche un message dans la console
+
+var cardname = "Chooixpeau"
