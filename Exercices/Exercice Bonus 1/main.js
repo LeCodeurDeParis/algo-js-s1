57b28f864ec4e101d2fc6dc3f0a6b8ef0aa3b71d
LeCodeurDeParis <Pl.Baillion@gmail.com>
2023-09-28 00:49:09 +0200

    Adding images for Exercice Bonus and creation of Pokemon

--- Exercices/Exercice Bonus 1/main.js
+++ Exercices/Exercice Bonus 1/main.js
@@ -4,27 +4,68 @@ function getRandomNumber(max) {
 
 var result = ""
 var fin = ""
+var cRes = 0
+var imageRes1 = document.createElement("img")
+var imageRes2 = document.createElement("img")
+var imageRes3 = document.createElement("img")
+
+var imageResC1 = document.createElement("img")
+var imageResC2 = document.createElement("img")
+var imageResC3 = document.createElement("img")
+
+
+imageRes1.src = "Pierre.png"
+imageRes2.src = "Ciseaux.png"
+imageRes3.src = "Feuille.png"
+
+imageResC1.src = "Pierre.png"
+imageResC2.src = "Ciseaux.png"
+imageResC3.src = "Feuille.png"
+
+var id1 = document.getElementById("ImagePlayer")
+var id2 = document.getElementById("ImageComputer")
 
 function deroulement(){
     var playerChoice = document.querySelector('input[name=Choice]:checked').value;
 
     var jOrdi = getRandomNumber(3)
+
     console.log(jOrdi)
+    console.log(id1)
+    console.log(id2)
+    
+
+    if (id1.hasChildNodes && id2.hasChildNodes()) {
+        while (id1.firstChild && id2.firstChild) {
+            id1.removeChild(id1.lastChild);
+            id2.removeChild(id2.lastChild);
+        }
+      }
+    
     if (playerChoice == "Papier") {
         if (jOrdi == 2){
             console.log("egal")
             result = "Papier"
             fin = "Égalité"
+            var cRes = 1
+            id1.appendChild(imageRes3)
+            id2.appendChild(imageResC3)
         }
         if (jOrdi == 0){
             console.log("gagné")
             result = "Pierre"
             fin = "Vous avez gagné !"
+            var cRes = 2
+            id1.appendChild(imageRes3)
+            id2.appendChild(imageResC1)
         }
         if (jOrdi == 1){
             console.log("perdu")
             result = "Ciseaux"
             fin = "Vous avez perdu..."
+            var cRes = 3
+            id1.appendChild(imageRes3)
+            id2.appendChild(imageResC2)
         }
     }
 
@@ -33,16 +74,25 @@ function deroulement(){
             console.log("egal")
             result = "Ciseaux"
             fin = "Égalité"
+            var cRes = 1
+            id1.appendChild(imageRes2)
+            id2.appendChild(imageResC2)
         }
         if (jOrdi == 0){
             console.log("gagné")
             result = "Papier"
             fin = "Vous avez gagné !"
+            var cRes = 2
+            id1.appendChild(imageRes2)
+            id2.appendChild(imageResC3)
         }
         if (jOrdi == 2){
             console.log("perdu")
             result = "Pierre"
             fin = "Vous avez perdu..."
+            var cRes = 3
+            id1.appendChild(imageRes2)
+            id2.appendChild(imageResC1)
         }
     }
 
@@ -51,18 +101,52 @@ function deroulement(){
             console.log("egal")
             result = "Pierre"
             fin = "Égalité"
+            var cRes = 1
+            id1.appendChild(imageRes1)
+            id2.appendChild(imageResC1)
         }
         if (jOrdi == 1){
             console.log("gagné")
             result = "Ciseaux"
             fin = "Vous avez gagné !"
+            var cRes = 2
+            id1.appendChild(imageRes1)
+            id2.appendChild(imageResC2)
         }
         if (jOrdi == 2){
             console.log("perdu")
             result = "Papier"
             fin = "Vous avez perdu..."
+            var cRes = 3
+            id1.appendChild(imageRes1)
+            id2.appendChild(imageResC3)
         }
+    }
+
+    if (cRes == 2){
+        document.body.style.background ='green'
+    }
+
+    if (cRes == 1){
+        document.body.style.background ='gray'
+
+    }
+    if (cRes == 3){
+        document.body.style.background ='red'
     }
     Choix.innerHTML = result
     Concl.innerHTML = fin
+
 }
+
+
+/*id.parentNode.removeChild(imageRes1)
+    id.parentNode.removeChild(imageRes2)
+    id.parentNode.removeChild(imageRes3)
+
+    if (id.hasChildNodes()) {
+        while (id.firstChild) {
+            id.removeChild(id.lastChild);
+        }
+      }
+      */
---
+++ Exercices/Pokemon/main.js
@@ -0,0 +1,73 @@
+/*class hero {
+    constructor(name, pv, atk, def){
+        this.name = name
+        this.pv = pv
+        this.atk = atk
+        this.def = def
+    }
+
+    attackMonster(monster){
+        monster.hp -= this.atk
+    }
+}
+
+class monster {
+    constructor(name, pv){
+        this.name = name
+        this.hp = hp
+    }
+}
+let samy = new hero("Samy", 100, 5, 7)
+let gnome = new monster("Gnome", 100)
+
+samy.attackMonster(gnome)*/
+
+boucleJeu = true
+var attackPokemon = 0
+
+class Pokémon{
+    constructor(name, attack, defense, hp, luck){
+        this.name = name
+        this.hp = hp
+        this.attack = attack
+        this.defense = defense
+        this.luck = luck
+    }
+    
+
+    attackPokemon(Pokémon){
+        Pokémon.hp += Pokémon.defense
+        Pokémon.hp -= this.attack
+        attackPokemon = this.attack - Pokémon.defense
+    }
+}
+
+let Ralpho = new Pokémon("Ralpho", 10, 4, 20, 2)
+let Ybois = new Pokémon("Ybois", 8, 5, 25, 2)
+/*
+Ralpho.atkReçu(Ybois)
+console.log(Ybois)
+console.log(Ralpho) 
+console.log("Ybois à prit", atkReçu, "de dégats, il lui reste ", Ybois.hp, "HP")
+*/
+while (boucleJeu == true){
+    Ralpho.attackPokemon(Ybois)
+    console.log("Ybois à prit", attackPokemon, "de dégats, il lui reste ", Ybois.hp, "HP")
+    if (Ybois.hp <= 0){
+        console.log(Ybois.name, "a perdu")
+        boucleJeu = false
+        break
+    }
+    
+    
+    Ybois.attackPokemon(Ralpho)
+    console.log("Ralpho à prit", attackPokemon, "de dégats, il lui reste ", Ralpho.hp, "HP")
+    if (Ralpho.hp <= 0){
+        console.log(Ralpho.name, "a perdu")
+        boucleJeu = false
+        break
+    }
+
+    
+}
+
